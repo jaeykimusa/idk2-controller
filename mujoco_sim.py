e8111b7485e3827370667785ef18cd30c54eddb4
Jaey Kim <jaeykimusa@gmail.com>
2026-01-21 09:38:51 -0500

    [Implementation] Fixed scene const, sim works via MuJoCoSim obj.

--- mujoco_sim.py
+++ mujoco_sim.py
@@ -5,7 +5,6 @@
 
 from robot_description.go2_description import go2_mujoco
 
-
 import mujoco as mj
 import mujoco.viewer
 
@@ -14,10 +13,10 @@ class MuJoCoSim:
 
 
     def __init__(this, dt=0.001):
-        this.mj_model = go2_mujoco.model
-        this.mj_data = go2_mujoco.data
+        this.model = go2_mujoco.model
+        this.data = go2_mujoco.data
         this.dt = dt
-        this.mj_model.opt.timestep = dt
+        this.model.opt.timestep = dt
         this.viewer = None
 
 
@@ -45,7 +44,7 @@ class MuJoCoSim:
             this.viewer.sync()
 
 
-    def launch_viewer(this):
+    def launch_viewer(this, contactPts=False, jointAxes=False, robotTransprt=False):
         this.viewer = mujoco.viewer.launch_passive(
             this.model, this.data
         )
@@ -56,17 +55,29 @@ class MuJoCoSim:
         this.viewer.cam.distance = 3
         this.viewer.cam.lookat[:] = [0, 0, 0.3]
 
+        # Show contact points
+        this.viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_JOINT] = contactPts
+        
+        # Show joint axes
+        this.viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = jointAxes
 
-    def run(this, controller):
-        this.launch_viewer()
+        # Make the robot slightly transparent to see internal joints
+        this.viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_TRANSPARENT] = robotTransprt
+
+
+    def run(this, tau):
+        this.launch_viewer(True, True, True)
 
         with this.viewer:
             while this.viewer.is_running():
                 state = this.get_state()
-                tau = controller.compute_control(state)
+                # tau = controller.compute_control(state)
                 this.apply_control(tau)
                 this.step()
                 this.render()
+
+    def close(this):
+        this.viewer.close()
 
 
 def main():
--- sim.py
+++ sim.py
@@ -4,4 +4,17 @@
 # date: 01/19/2026
 
 
+from mujoco_sim import MuJoCoSim
+
+import numpy as np
+import mujoco as mj
+
+
+zero_torques = np.zeros(12)
+# zero_torques = mj.mju_zero(0,12)
+mj_sim = MuJoCoSim()
+
+# mj_sim.launch_viewer()
+
+mj_sim.run(zero_torques)
 
